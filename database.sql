bd6ddf6d27c7644c584df68754768026606811c5
Oğuzhan Taçtan <46655443+oguzhantctn09@users.noreply.github.com>
2021-12-27 15:10:05 +0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,4 @@
+--CREATE DATABASE kullanicilar;
+
+--CREATE TABLE kullanici_liste VALUES (	user_id serial PRIMARY KEY, name VARCHAR ( 70) ,  email VARCHAR ( 255 ), academic VARCHAR(100), password VARCHAR ( 70 ));
+
